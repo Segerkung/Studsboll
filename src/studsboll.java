6cbae8e8fa376a502c687aa40af6e573f78692dc
Segerkung <60388987+Segerkung@users.noreply.github.com>
2021-09-06 09:35:36 +0200

    testade massa men funkade inte, så gick tillbaka till detta

--- src/studsboll.java
+++ src/studsboll.java
@@ -2,13 +2,19 @@ public class studsboll {
     public static void main(String[] args) {
 
         double h = 100;
-        double g = -9.82;
         double v = 0;
+        double a = -9.82;
+        double t = 1;
+
+        v = 1/2 * a * t*2;
 
 
+        while(true){
+        System.out.println(v);
 
     }
 
-}
+}}
+
 
 
